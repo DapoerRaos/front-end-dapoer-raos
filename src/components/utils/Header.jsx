a527e3e09d67bcccbec3a13540525450c73bf3b7
bhanurz <aswanggab@gmail.com>
2024-04-19 19:36:58 +0700

    refactor: move header file component to utils folder components

---
+++ src/components/utils/Header.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+
+const Header = ({ title, linkHref, linkTitle, productLength }) => {
+  return (
+    <div className="py-4 mx-10 flex justify-between items-center">
+      <h1 className="text-3xl text-[#feab3b] font-bold">
+        {title} {productLength && ` (${productLength})`}
+      </h1>
+      {linkHref && linkTitle ? (
+        <Link
+          href={linkHref}
+          className="md:text-lg text-sm text-center text-[#feab3b] font-medium hover:text-[#febb3b] hover:pe-2 hover:underline transition-all"
+        >
+          {linkTitle}
+        </Link>
+      ) : null}
+    </div>
+  );
+};
+
+export default Header;
