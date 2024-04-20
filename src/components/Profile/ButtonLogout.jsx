fc3d7f95edba48b06fd3e87005a30f3bb1ac45df
bhanurz <aswanggab@gmail.com>
2024-04-20 14:49:07 +0700

    feat: add button logout for user profile page

---
+++ src/components/Profile/ButtonLogout.jsx
@@ -0,0 +1,72 @@
+import { logoutUser } from "@/libs/auth-libs";
+import {
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  useToast,
+  Flex,
+  Icon,
+} from "@chakra-ui/react";
+import { SignOut } from "@phosphor-icons/react";
+import { useRouter } from "next/navigation";
+
+const ButtonLogout = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const router = useRouter();
+  const toast = useToast();
+
+  const handleLogout = async () => {
+    await logoutUser();
+    router.push("/auth/login");
+    toast({
+      title: "Berhasil Logout",
+      status: "success",
+      duration: 2000,
+      position: "top",
+    });
+  };
+  return (
+    <Flex
+      onClick={onOpen}
+      align="center"
+      p="4"
+      mx="4"
+      borderRadius="lg"
+      role="group"
+      cursor="pointer"
+      transition={"all 0.3s"}
+      color="#1c2434"
+      _hover={{
+        bg: "red.500",
+        color: "white",
+      }}
+    >
+      <Icon mr="4" fontSize={20} as={SignOut} />
+      <Text fontWeight={"medium"}>Keluar</Text>
+      <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent alignItems={"center"}>
+          <ModalHeader>Apakah anda yakin ingin keluar?</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>Tekan &quot;Logout&quot; jika ingin keluar</Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={handleLogout} colorScheme={"red"}>
+              Logout
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Flex>
+  );
+};
+
+export default ButtonLogout;
