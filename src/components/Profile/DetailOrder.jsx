dc620e25a7be3509ae47ea2cbb43c7bb51ea3183
bhanurz <aswanggab@gmail.com>
2024-05-01 22:47:47 +0700

    feat: add customer data on order summary parameter

--- src/components/Profile/DetailOrder.jsx
+++ src/components/Profile/DetailOrder.jsx
@@ -9,12 +9,14 @@ import { getOrderStatusById } from "@/libs/order-libs";
 const DetailOrder = ({ token, order_id, id }) => {
   const [data, setData] = useState([]);
   const [orderItems, setOrderItems] = useState([]);
+  const [customer, setCustomer] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
       const orderData = await getOrderStatusById(token, order_id);
       setData(orderData.data);
       setOrderItems(orderData.data.OrderItems);
+      setCustomer(orderData.data.Customer);
     };
     fetchData();
   }, [token, order_id]);
@@ -28,7 +30,12 @@ const DetailOrder = ({ token, order_id, id }) => {
       />
       <div className="lg:grid lg:grid-cols-10 lg:items-start gap-x-10 mx-10">
         <div className="rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8 shadow-xl border">
-          <OrderSummary token={token} data={data} orderItems={orderItems} />
+          <OrderSummary
+            token={token}
+            data={data}
+            orderItems={orderItems}
+            customerData={customer}
+          />
         </div>
         <div className="lg:col-span-5 lg:overflow-y-scroll lg:max-h-[460px]">
           <OrderItems orderItems={orderItems} />
