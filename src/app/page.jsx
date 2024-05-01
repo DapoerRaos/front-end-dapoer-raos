1356c2c24871d1287cc72d429c2a0726df3d8429
bhanurz <aswanggab@gmail.com>
2024-05-01 22:45:37 +0700

    feat: change navbar component to main layout and add token parameter on product list

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,31 @@
 import Hero from "@/components/Layouts/Hero";
-import Navbar from "@/components/Layouts/Navbar";
-import ProducList from "@/components/ProductList";
-import Header from "@/components/ProductList/Header";
+import MainLayout from "@/components/Layouts/MainLayout";
+import ProductList from "@/components/Product/ProductList";
+import ButtonAllProduct from "@/components/Product/utils/ButtonAllProduct";
+import Header from "@/components/utils/Header";
 import { getProducts } from "@/libs/product-libs";
 import { cookies } from "next/headers";
 
 export default async function Home() {
   const cookieStore = cookies();
-  const token = cookieStore.get("token");
+  const token = cookieStore.get("token")?.value;
 
   const productList = await getProducts();
 
   return (
-    <>
-      <Navbar token={token?.value} />
+    <MainLayout token={token}>
       <section>
         <Hero />
       </section>
-      <section className="py-4">
+      <section className="pb-6">
         <Header
           title={"Katalog Produk"}
           linkHref={"/products"}
           linkTitle={"Lihat Semua"}
         />
-        <ProducList api={productList} />
+        <ProductList api={productList} token={token} />
+        <ButtonAllProduct />
       </section>
-    </>
+    </MainLayout>
   );
 }
