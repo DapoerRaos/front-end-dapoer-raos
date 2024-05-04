5a4f51d5ed014a7e19b3fd950c3623ddd030f808
bhanurz <aswanggab@gmail.com>
2024-05-04 17:26:00 +0700

    feat: add table of product list

--- src/components/Admin/Product/ProductTable.jsx
+++ src/components/Admin/Product/ProductTable.jsx
@@ -11,40 +11,42 @@ import {
   Td,
   useToast,
 } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import DropdownAction from "./utils/DropdownAction";
 import { getProducts } from "@/libs/product-libs";
-import Pagination from "./utils/Pagination";
+import Pagination from "@/components/utils/Pagination";
 
-const ProductTable = ({ seacrhKeyword, token }) => {
+const ProductTable = ({ searchKeyword, token }) => {
   const [productData, setProductData] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const toast = useToast();
 
-  const fetchData = async () => {
-    try {
-      const response = await getProducts(page, seacrhKeyword);
-      setProductData(response.data);
-      setTotalPages(response.pagination.totalPages);
-    } catch (err) {
-      failedToast(err.response.data.message);
-    }
-  };
+  const failedToast = useCallback(
+    (message) => {
+      toast({
+        title: message,
+        status: "error",
+        duration: 3000,
+        position: "top",
+        isClosable: true,
+      });
+    },
+    [toast]
+  );
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getProducts(page, searchKeyword);
+        setProductData(response.data);
+        setTotalPages(response.pagination.totalPages);
+      } catch (err) {
+        failedToast(err.response.data.message);
+      }
+    };
     fetchData();
-  }, [page, seacrhKeyword]);
-
-  const failedToast = (message) => {
-    toast({
-      title: message,
-      status: "error",
-      duration: 3000,
-      position: "top",
-      isClosable: true,
-    });
-  };
+  }, [page, searchKeyword, failedToast]);
 
   return (
     <>
@@ -54,7 +56,6 @@ const ProductTable = ({ seacrhKeyword, token }) => {
             <Tr>
               <Th>No</Th>
               <Th>Nama</Th>
-              <Th>Kategori</Th>
               <Th>Harga</Th>
               <Th>Stok</Th>
               <Th>Action</Th>
@@ -73,7 +74,6 @@ const ProductTable = ({ seacrhKeyword, token }) => {
                   <Tr key={index} color={"gray.600"}>
                     <Td>{index + 1}</Td>
                     <Td>{product.name}</Td>
-                    <Td>{product.category_name}</Td>
                     <Td>{formatPrice(product.price)}</Td>
                     <Td>{product.stock}</Td>
                     <Td>
