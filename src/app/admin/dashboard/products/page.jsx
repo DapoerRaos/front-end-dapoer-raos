054d7ae615a1c5a0bf7e069a185c4a1a644a0587
bhanurz <aswanggab@gmail.com>
2024-05-07 11:57:47 +0700

    fix: remove table panels and change to product control component

--- src/app/admin/dashboard/products/page.jsx
+++ src/app/admin/dashboard/products/page.jsx
@@ -1,13 +1,11 @@
 import DashboardLayout from "@/components/Layouts/DashboardLayout";
 import ProductControl from "@/components/Admin/Product/ProductControl";
 import {
-  Box,
   Tabs,
   TabList,
   Tab,
   TabPanels,
   TabPanel,
-  Heading,
   TabIndicator,
 } from "@chakra-ui/react";
 import { jwtDecode } from "jwt-decode";
@@ -16,10 +14,10 @@ import { redirect } from "next/navigation";
 
 const Page = async () => {
   const cookieStore = cookies();
-  const token = cookieStore.get("token");
+  const token = cookieStore.get("token")?.value;
 
   if (token) {
-    const decodedToken = jwtDecode(token.value);
+    const decodedToken = jwtDecode(token);
     if (decodedToken.role !== "admin") {
       redirect("/");
     }
@@ -28,36 +26,8 @@ const Page = async () => {
   }
 
   return (
-    <DashboardLayout>
-      <Box ml={{ base: 0, md: 60 }} p="6">
-        <div className="w-50 h-50">
-          <div className="mb-3">
-            <Heading color={"#feab3b"}>Kelola Produk</Heading>
-          </div>
-          <Tabs size="md" position={"relative"} variant="unstyled">
-            <TabList>
-              <Tab>Produk</Tab>
-              <Tab>Stok</Tab>
-            </TabList>
-            <TabIndicator
-              mt="-1.5px"
-              height="2px"
-              bg="#feab3b"
-              borderRadius="1px"
-            />
-            <TabPanels>
-              <TabPanel>
-                <ProductControl token={token.value} />
-              </TabPanel>
-              <TabPanel>
-                <div>
-                  <p>stok produk</p>
-                </div>
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
-        </div>
-      </Box>
+    <DashboardLayout title={"Kelola Produk"}>
+      <ProductControl token={token} />
     </DashboardLayout>
   );
 };
