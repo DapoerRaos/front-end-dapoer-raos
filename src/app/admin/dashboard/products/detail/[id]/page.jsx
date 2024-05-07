3599f60e13e8a8afa38dde71a0ff424171fdb268
bhanurz <aswanggab@gmail.com>
2024-05-07 11:59:27 +0700

    refactor: remove box element andd add title props for dashboard layout component

--- src/app/admin/dashboard/products/detail/[id]/page.jsx
+++ src/app/admin/dashboard/products/detail/[id]/page.jsx
@@ -1,17 +1,15 @@
-import DetailProductCard from "@/components/Admin/Product/Detail";
+import DetailProductCard from "@/components/Admin/Product/DetailCard";
 import DashboardLayout from "@/components/Layouts/DashboardLayout";
-import { Box, Heading, Text } from "@chakra-ui/react";
 import { jwtDecode } from "jwt-decode";
 import { cookies } from "next/headers";
-import Link from "next/link";
 
 const Page = async ({ params }) => {
   const { id } = params;
   const cookieStore = cookies();
-  const token = cookieStore.get("token");
+  const token = cookieStore.get("token")?.value;
 
   if (token) {
-    const decodedToken = jwtDecode(token.value);
+    const decodedToken = jwtDecode(token);
     if (decodedToken.role !== "admin") {
       redirect("/");
     }
@@ -20,24 +18,12 @@ const Page = async ({ params }) => {
   }
 
   return (
-    <DashboardLayout>
-      <Box ml={{ base: 0, md: 60 }} p="8">
-        <div className="w-50 h-50">
-          <div className="mb-3 flex items-center justify-between">
-            <Heading color={"#feab3b"}>Detail Produk</Heading>
-            <Link href={"/admin/dashboard/products"}>
-              <Text
-                color={"#feab3b"}
-                transition={"all 0.2s"}
-                _hover={{ textDecoration: "underline", pr: 2 }}
-              >
-                Kembali
-              </Text>
-            </Link>
-          </div>
-          <DetailProductCard id={id} token={token.value} />
-        </div>
-      </Box>
+    <DashboardLayout
+      title={"Detail Produk"}
+      linkTitle={"Kembali"}
+      linkHref={"/admin/dashboard/products"}
+    >
+      <DetailProductCard id={id} token={token} />
     </DashboardLayout>
   );
 };
