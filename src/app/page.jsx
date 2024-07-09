5fcbc8fa92ff9b26e3d559ffcfdf1f8a7e28e5fb
bhanurz <aswanggab@gmail.com>
2024-07-09 09:27:12 +0700

    feat: add testimonial component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Layouts/Hero";
 import MainLayout from "@/components/Layouts/MainLayout";
+import Testimonial from "@/components/Layouts/Testimonial";
 import ProductList from "@/components/Product/ProductList";
 import ButtonAllProduct from "@/components/Product/utils/ButtonAllProduct";
 import Header from "@/components/utils/Header";
@@ -16,6 +17,9 @@ export default async function Home() {
     <MainLayout token={token}>
       <section>
         <Hero />
+      </section>
+      <section className="pb-4">
+        <Testimonial />
       </section>
       <section className="pb-6">
         <Header
