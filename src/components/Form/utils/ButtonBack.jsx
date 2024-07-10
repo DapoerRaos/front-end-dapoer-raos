fe283770193d48b4051e5f9d0111d7d1c1f3c35e
bhanurz <aswanggab@gmail.com>
2024-07-10 11:47:57 +0700

    fix: router for button back

--- src/components/Form/utils/ButtonBack.jsx
+++ src/components/Form/utils/ButtonBack.jsx
@@ -2,26 +2,28 @@
 
 import { ChevronLeftIcon } from "@chakra-ui/icons";
 import { Box, Text } from "@chakra-ui/react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const ButtonBack = () => {
+  const router = useRouter();
+  const handleButton = () => {
+    router.back();
+  };
   return (
-    <Link href="/">
-      {" "}
-      <Box
-        _hover={{ cursor: "pointer", paddingLeft: 2, color: "#feab3b" }}
-        transition={"all 0.2s"}
-        position="absolute"
-        top={4}
-        left={4}
-        display={"flex"}
-        alignItems={"center"}
-      >
-        <ChevronLeftIcon boxSize={6} />
-        <Text>Kembali</Text>
-      </Box>
-    </Link>
+    <Box
+      _hover={{ cursor: "pointer", paddingLeft: 2, color: "#feab3b" }}
+      transition={"all 0.2s"}
+      position="absolute"
+      top={4}
+      left={4}
+      display={"flex"}
+      alignItems={"center"}
+      onClick={() => handleButton()}
+    >
+      <ChevronLeftIcon boxSize={6} />
+      <Text>Kembali</Text>
+    </Box>
   );
 };
 
