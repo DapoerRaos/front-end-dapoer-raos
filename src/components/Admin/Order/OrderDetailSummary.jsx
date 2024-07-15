0c16f229646524d517a4825fcb9a580ee9abca2c
bhanurz <aswanggab@gmail.com>
2024-07-15 11:10:04 +0700

    fix: order detail logic

--- src/components/Admin/Order/OrderDetailSummary.jsx
+++ src/components/Admin/Order/OrderDetailSummary.jsx
@@ -6,70 +6,78 @@ import { useToast } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import ModalDetailCustomer from "./ModalDetailCustomer";
 import { getCustomerDetailById } from "@/libs/customer-libs";
+import { updateProductStock } from "@/libs/product-libs";
+import { formatDate } from "date-fns";
 
 const OrderDetailSummary = ({ token, data, orderItems }) => {
   const [expireTime, setExpireTime] = useState("");
   const [customer, setCustomer] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
   const toast = useToast();
 
   useEffect(() => {
     const fetchData = async () => {
-      const userCustomer = await getCustomerDetailById(token, data.customer_id);
-      const response = await getTransactionStatus(token, data.id);
-      setCustomer(userCustomer.data);
-      setExpireTime(response.data.expiry_time);
+      try {
+        const userCustomer = await getCustomerDetailById(
+          token,
+          data.customer_id
+        );
+        const response = await getTransactionStatus(token, data.id);
+        setCustomer(userCustomer.data);
+        setExpireTime(response.data.expiry_time);
+      } catch (err) {
+        console.log(err);
+      }
     };
 
     fetchData();
   }, [token, data.id, data.customer_id]);
 
   const handleReCheckStatus = async (id) => {
-    const response = await getTransactionStatus(token, id);
-    if (response.data.transaction_status === "settlement") {
-      const updateData = {
-        newStatus: "Paid",
-        shipping_status: "Packaged",
-      };
-      await updateOrderStatus(token, id, updateData);
-      location.reload();
-      toast({
-        title: "Pesanan Sudah Terbayar",
-        status: "success",
-        duration: 2000,
-        position: "top",
-      });
-    } else if (response.data.transaction_status === "expire") {
-      const stock_product = {
-        stock_products: orderItems.map((item) => ({
-          product_id: item.product_id,
-          stock: item.Product.stock + item.quantity,
-        })),
-      };
-      await updateOrderStatus(token, id, { newStatus: "Canceled" });
-      await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products/stock`, {
-        method: "PUT",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Cookie: `token=${cookieToken}`,
-        },
-        credentials: "include",
-        body: JSON.stringify(stock_product),
-      });
-      location.reload();
-      toast({
-        title: "Pesanan Sudah Kadaluarsa, Lakukan Pesananan Ulang",
-        status: "error",
-        duration: 2000,
-        position: "top",
-      });
-    } else {
-      toast({
-        title: "Pesanan Belum Terbayar",
-        status: "error",
-        duration: 2000,
-        position: "top",
-      });
+    try {
+      setIsLoading(true);
+      const response = await getTransactionStatus(token, id);
+      if (response.data.transaction_status === "settlement") {
+        const updateData = {
+          newStatus: "Lunas",
+          shipping_status: "Packaged",
+        };
+        await updateOrderStatus(token, id, updateData);
+        location.reload();
+        toast({
+          title: "Pesanan Sudah Terbayar",
+          status: "success",
+          duration: 2000,
+          position: "top",
+        });
+      } else if (response.data.transaction_status === "expire") {
+        const stock_product = {
+          stock_products: orderItems.map((item) => ({
+            product_id: item.product_id,
+            stock: item.Product.stock + item.quantity,
+          })),
+        };
+        await updateOrderStatus(token, id, { newStatus: "Canceled" });
+        await updateProductStock(token, stock_product);
+        location.reload();
+        toast({
+          title: "Pesanan Sudah Kadaluarsa, Lakukan Pesananan Ulang",
+          status: "error",
+          duration: 2000,
+          position: "top",
+        });
+      } else {
+        toast({
+          title: "Pesanan Belum Terbayar",
+          status: "error",
+          duration: 2000,
+          position: "top",
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -82,6 +90,7 @@ const OrderDetailSummary = ({ token, data, orderItems }) => {
               <p className="text-sm font-medium">Re-check Status Pembayaran</p>
             </div>
             <button
+              disabled={isLoading}
               onClick={() => handleReCheckStatus(data.id)}
               className="text-sm py-1 px-2 font-medium rounded-lg text-[#feab3b] bg-orange-100 hover:bg-[#feab3b] hover:text-white transition-all"
             >
@@ -90,7 +99,7 @@ const OrderDetailSummary = ({ token, data, orderItems }) => {
           </div>
         </div>
       )}
-      <ModalDetailCustomer customerData={customer} />
+      <ModalDetailCustomer data={data} customerData={customer} />
       <div>
         <h2 className="text-lg font-medium text-gray-900">Informasi Pesanan</h2>
         <div className="mt-2 flex justify-between">
@@ -121,7 +130,7 @@ const OrderDetailSummary = ({ token, data, orderItems }) => {
           <p className="text-sm">Status Pembayaran</p>
           <p
             className={`text-sm font-medium uppercase ${
-              data.status === "Paid"
+              data.status === "Lunas"
                 ? "text-green-500 bg-green-100 p-1 rounded-lg"
                 : data.status === "Pending"
                 ? "text-blue-500 bg-blue-100 p-1 rounded-lg"
@@ -133,51 +142,29 @@ const OrderDetailSummary = ({ token, data, orderItems }) => {
             {data.status}
           </p>
         </div>
-        <div className="mt-2 flex justify-between">
-          <p className="text-sm">Metode Pengiriman</p>
-          <p className="text-sm font-medium">{data.shipping_type}</p>
-        </div>
-        {/* {data.payment_method !== "COD" && data.shipping_type !== "Pick Up" && ( */}
-        <div className="mt-2 flex justify-between items-center">
-          <p className="text-sm">Status Pengiriman</p>
-          <p className="text-sm font-medium">
-            {data.shipping_status === "Waiting"
-              ? "Menunggu Pembayaran"
-              : data.shipping_status === "Packaged"
-              ? "Barang di Kemas"
-              : data.shipping_status === "Ready"
-              ? "Barang Siap Diambil"
-              : data.shipping_status === "Delivered"
-              ? "Barang Dikirim"
-              : data.shipping_status === "Picked Up"
-              ? "Barang Telah Diambil"
-              : data.shipping_status === "Received" && "Barang Telah Diterima"}
-          </p>
-        </div>
-        {/* )} */}
         {data.status === "Pending" &&
           data.payment_method === "bank_transfer" && (
             <div className="mt-2 flex justify-between items-center">
               <p className="text-sm">Lakukan Pembayaran Sebelum</p>
-              <p className="text-sm font-medium uppercase">{expireTime}</p>
+              <p className="text-sm font-medium">{expireTime}</p>
             </div>
           )}
         <div className="mt-2 flex justify-between items-center">
           <p className="text-sm">Tanggal Transaksi</p>
-          <p className="text-sm font-medium uppercase">{data.payment_date}</p>
+          <p className="text-sm font-medium">{data.payment_date}</p>
         </div>
-        {data.shipping_type === "Delivery" && (
+        {data.Shipping?.type === "Delivery" && (
           <div className="mt-2 flex justify-between items-center">
             <p className="text-sm">Ongkos Kirim</p>
             <p className="text-sm font-medium uppercase">
-              {formatPrice(data.shipping_cost)}
+              {formatPrice(data.Shipping?.cost)}
             </p>
           </div>
         )}
         <div className="mt-2 flex justify-between items-center">
           <p className="text-sm">Total Harga Barang</p>
           <p className="text-sm font-medium uppercase">
-            {formatPrice(data.total_price - data.shipping_cost)}
+            {formatPrice(data.total_price - data.Shipping?.cost)}
           </p>
         </div>
       </div>
