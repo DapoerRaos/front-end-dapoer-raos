64e527188bbe47e849e4dd32c3b9324b7a994e94
bhanurz <aswanggab@gmail.com>
2024-07-15 11:06:04 +0700

    feat: add redirect if no token

--- src/app/profile/user/[id]/page.jsx
+++ src/app/profile/user/[id]/page.jsx
@@ -2,10 +2,14 @@ import ProfileLayout from "@/components/Layouts/ProfileLayout";
 import TransactionSearhInput from "@/components/Profile/TransactionSearhInput";
 import { getCustomerById } from "@/libs/customer-libs";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const page = async () => {
   const cookieStore = cookies();
   const token = cookieStore.get("token")?.value;
+  if (!token) {
+    redirect("/");
+  }
 
   const userCustomer = await getCustomerById(token);
 
