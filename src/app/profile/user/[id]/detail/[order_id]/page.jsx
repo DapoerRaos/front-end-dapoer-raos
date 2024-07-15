861d041f411ef94bde7ec35e33a5276cacee9aef
bhanurz <aswanggab@gmail.com>
2024-07-15 11:06:24 +0700

    feat: add redirect if no token

--- src/app/profile/user/[id]/detail/[order_id]/page.jsx
+++ src/app/profile/user/[id]/detail/[order_id]/page.jsx
@@ -1,11 +1,15 @@
 import MainLayout from "@/components/Layouts/MainLayout";
 import DetailOrder from "@/components/Profile/DetailOrder";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const page = ({ params }) => {
   const { order_id, id } = params;
   const cookieStore = cookies();
   const token = cookieStore.get("token")?.value;
+  if (!token) {
+    redirect("/");
+  }
   return (
     <MainLayout token={token}>
       <DetailOrder token={token} order_id={order_id} id={id} />
